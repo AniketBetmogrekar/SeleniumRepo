845614ddd336c9e35090d046f75b0195dc457342
Aniket Betmogrekar <aniketbetmogrekar595@gmail.com>
2026-03-17 12:03:04 +0530

    Initial Selenium login automation project

---
+++ src/LoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        String msg = driver.findElement(By.id("flash")).getText();
+
+        if(msg.contains("You logged into a secure area!")){
+            System.out.println("Login Successful ✅");
+        } else {
+            System.out.println("Login Failed ❌");
+        }
+
+        driver.quit();
+    }
+}
